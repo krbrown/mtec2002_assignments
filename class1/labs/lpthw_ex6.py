dd1fce5cbcbcbfe7c7547052e2d5bb4573267e48
krbrown <krystinebrownwd@gmail.com>
2012-02-01 20:36:40 -0500

    Update class1/labs/lpthw_ex6.py

---
+++ class1/labs/lpthw_ex6.py
@@ -0,0 +1,23 @@
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+
+print "I said: %r." % x
+print "I also said: '%s'." % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
+#the first 4 lines explain the variable
+#I'm assuming the 4 lines mentioned in the extra credit are print "I said: %r." % x
+#print "I also said: '%s'." % y dunno wher ethe others are.
